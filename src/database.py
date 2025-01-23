10e7001a934c76f15890af84379f178592af2738
B0ond <amiram19@yandex.ru>
2025-01-23 22:58:40 +0300

    test db

--- src/database.py
+++ src/database.py
@@ -1,3 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy.ext.declarative import declarative_base
+
+URL_DATABASE = 'postgresql://postgres:@localhost:5432/misa_db'
+
+engine = create_engine(URL_DATABASE)
+
+Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
--- src/main.py
+++ src/main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI, HTTPException, Depends
 from pydantic import BaseModel
 from typing import List, Annotated
+import models
+from database import engine, SessionLocal
+from sqlalchemy.orm import Session
 
 app = FastAPI()
-
+models.Base.metadata.create_all(bind=engine)
 
 class ChoiceBase(BaseModel):
     choice_text: str
@@ -13,3 +16,20 @@ class QuestionBase(BaseModel):
     question_text: str
     choices: List[ChoiceBase]
     
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependecy = Annotated[Session, Depends(get_db)]
+
+@app.post("/questions/")
+async def create_question(question: QuestionBase, db:db_dependecy):
+    db_qsession = models.Questions(question_text=question.question_text)
+    db.add(db_qsession)
+    db.commit()
+    db.refresh(db_qsession)
+    for choice in question.choices:
+        db_choice = models.Choises(choice_text=choice.choice_text, is_correct=choice.is_correct, question_id=db_qsession.id)
