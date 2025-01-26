6b83681db8cf48992511805e8222e6c9f56b9b2b
B0ond <amiram19@yandex.ru>
2025-01-26 21:49:01 +0300

    done

--- app/database.py
+++ app/database.py
@@ -1,15 +1,9 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
-from dotenv import load_dotenv
-import os
 
-load_dotenv()
+DATABASE_URL = "postgresql+asyncpg://postgres:password@db:5432/postgres"
 
-SQLALCHEMY_DATABASE_URL = os.getenv('SQLALCHEMY_DATABASE_URL')
-
-# SQLALCHEMY_DATABASE_URL = "postgresql+asyncpg://postgres:AAAaaa111@localhost/misa_db"
-
-engine = create_async_engine(SQLALCHEMY_DATABASE_URL, echo=True)
+engine = create_async_engine(DATABASE_URL, echo=True)
 AsyncSessionLocal = sessionmaker(
     bind=engine,
     class_=AsyncSession,
--- app/main.py
+++ app/main.py
@@ -2,9 +2,9 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI, HTTPException, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
-from database import async_get_db, engine
-from schemas import DoctorSchema, PatientSchema, ClinicSchema, AppointmentSchema
-from models import Doctor, Patient, Base, Clinic, Appointment
+from .database import async_get_db, engine
+from .schemas import DoctorSchema, PatientSchema, ClinicSchema, AppointmentSchema
+from .models import Doctor, Patient, Base, Clinic, Appointment
 import logging
 
 logging.basicConfig(level=logging.INFO)
