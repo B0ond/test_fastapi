b1b093732b4c13c0b874dd40f6f6165daf66c6b6
B0ond <amiram19@yandex.ru>
2025-01-26 17:08:01 +0300

    docker files

--- app/database.py
+++ app/database.py
@@ -1,7 +1,13 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
+import os
 
-SQLALCHEMY_DATABASE_URL = "postgresql+asyncpg://postgres:AAAaaa111@localhost/misa_db"
+load_dotenv()
+
+SQLALCHEMY_DATABASE_URL = os.getenv('SQLALCHEMY_DATABASE_URL')
+
+# SQLALCHEMY_DATABASE_URL = "postgresql+asyncpg://postgres:AAAaaa111@localhost/misa_db"
 
 engine = create_async_engine(SQLALCHEMY_DATABASE_URL, echo=True)
 AsyncSessionLocal = sessionmaker(
