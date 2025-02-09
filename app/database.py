31944928f0c872b3d8720d1ced3532b482fec228
B0ond <amiram19@yandex.ru>
2025-02-09 22:30:44 +0300

    в тестах надо бы еще разобраться

--- app/database.py
+++ app/database.py
@@ -3,21 +3,32 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv
 
-
 load_dotenv()
 
+# Получаем конфигурацию базы данных
 DB_TYPE = os.getenv("DB_TYPE")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_HOST = os.getenv("DB_HOST")
+DB_HOST = os.getenv("DB_HOST", "db")  # Значение по умолчанию для локальной разработки
 DB_PORT = os.getenv("DB_PORT")
 DB_NAME = os.getenv("DB_NAME")
+
+# Используем переменную окружения TESTING для определения, использовать ли тестовую базу данных
+TESTING = os.getenv("TESTING") == "True"
+print(f"TESTING: {TESTING}")
 
 DATABASE_URL = (
     f"postgresql{DB_TYPE}://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 )
+TEST_DATABASE_URL = os.getenv("TEST_DATABASE_URL")  # Получаем из .env
 
-engine = create_async_engine(DATABASE_URL, echo=True)
+# Используем переменную для подключения к базе данных
+DATABASE_URL_TO_USE = TEST_DATABASE_URL if TESTING and TEST_DATABASE_URL else DATABASE_URL
+print(f"Using database URL: {DATABASE_URL_TO_USE}")
+
+
+# Настроим engine и сессии
+engine = create_async_engine(DATABASE_URL_TO_USE, echo=True)
 AsyncSessionLocal = sessionmaker(
     bind=engine,
     class_=AsyncSession,
@@ -25,10 +36,9 @@ AsyncSessionLocal = sessionmaker(
 )
 Base = declarative_base()
 
-
 async def async_get_db():
-    async with AsyncSessionLocal() as db:  # Создаём асинхронную сессию
+    async with AsyncSessionLocal() as db:
         try:
-            yield db  # Передаём её в зависимости
+            yield db
         finally:
-            await db.close()  # Закрываем сессию после использования
+            await db.close()
